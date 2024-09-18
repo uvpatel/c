82e8e550558bbb2d8720cc6f3249d1f51a4e068d
uvpatel <uvpatel7271@gmail.com>
2024-09-18 07:32:50 -0700

    Add files via upload

---
+++ know.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+int main(){   
+//   a) int a = 8^8 is !=  64 here, ^ is xor (bitwise op)  // valid
+/* b).int x;int y = x; //valid
+but int x ,int y =x;  // not valid
+
+when we make char so we have to input only 1 char 
+Ex. char == "**"  // not valid
+
+
+*/
+
+return 0;
+}
---
+++ ternary.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+int main(){   
+//Conditional Operator
+/*Ternary
+Syntax :
+Condition? doSomething if TRUE:doSomething if FALSE;
+? check your conditon is true or not
+if true print : before otherwise  after :
+*/
+//usage
+
+  int age;
+  printf("Enter age: ");
+  scanf("%d",&age);
+  age >= 18? printf("Adult  \n") : printf("not Adult\n");
+// not more usage but synatax is good if u want to perform some small conditional so u can use it and do not terminate beetween
+
+return 0;
+}
