620e56afb36b8840bf090d98d83a721d0cefbd9d
uvpatel <uvpatel7271@gmail.com>
2024-09-18 07:04:38 -0700

    Add files via upload

---
+++ code_execution.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+int main(){    
+    /*A compiler is a computer program which converts a C program into machine language
+so that it can be easily understood by the computer.*/
+
+
+// A C program is written in plain text.
+
+
+/*This plain text is combination of instructions in a particular sequence. The compiler
+performs some basic checks and finally converts the program into an executable.*/
+
+
+/*computer can't understand c language so we need compile that convert over code into 0 and 1 
+because computer understand binary language so it make exe file over file copiler
+this process of convertion called compilation*/
+
+    return 0;
+}
---
+++ instruction.c
@@ -0,0 +1,36 @@
+#include<stdio.h> 
+int main(){    
+// Instruction tell our programme what our programme should do.
+/*Type of instruction:
+1)Type declaration
+2)Arithmetic instruction
+3)Control instruction
+*/
+/*usage:
+1)when we declare any variable we use 1st type of instruction
+// valid statement;
+c languge's code compile line by line
+
+// in same line we can't use and declare varible 
+
+so when we don't decalair variabal we can't use it.
+
+we can declare multiple var in a single line
+
+
+
+2)Arithmetic instruction
+Arithmetic= when we perform action with oprends through operator
+a + b here, a and b are operand and + is operator
+value always asigine by R.H.S TO L.H.S
+valid            ||       invalide  
+a = b +c         ||        b+c =a
+a = b *c         ||        bc =a
+a = b /c         ||        a  = b^c*/
+
+
+
+  
+  
+    return 0;
+}
