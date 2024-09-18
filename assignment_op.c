15348f0a90b3e78182cc68667bbfb609a7316d8d
uvpatel <uvpatel7271@gmail.com>
2024-09-18 07:11:52 -0700

    Add files via upload

---
+++ assignment_op.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+int main(){    
+    /*
+    Assignment   = 
+    short hand Assignment  +=, -= ,*=,/= %=
+    */
+return 0;
+}
---
+++ operator_prcidence.c
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+int main(){    
+    /*
+    OPERATOR PRECEDENCE
+Priority         Operator
+ 1st             !
+ 2nd             *, /, %
+  3rd           +, -
+ 4th           <>, <=, >=
+ 5 th           ==, !=
+ 6th            &&
+ 7th            ||
+ 8 th =
+  
+
+     */
+return 0;
+}
